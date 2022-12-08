714a79bc0dc2a7e7ebfa02d74ebeb87e28fb6dbd
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2022-12-07 23:06:21 -0500

    first commit

---
+++ pages/Board.js
@@ -0,0 +1,14 @@
+import Card from "./card";
+
+const Board = () => {
+
+    return (
+        <div>
+            <h1>Memory game!</h1>
+            <Card />
+        </div>
+
+    )
+}
+
+export default Board;
---
+++ pages/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+    return (
+<div classname="card">
+<h3>card</h3>
+</div>
+    )
+}
+
+export default Card;
